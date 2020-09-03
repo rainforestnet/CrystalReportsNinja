11279c631691111bd9f6fb4286c74d96d7605cfd
Matthew Hertzfeld <matthew.hertzfeld@protonmail.com>
2020-09-03 17:43:36 -0400

    Testing Resources
    Added resources for testing.

---
+++ Testing/AdventureWorksLt/Readme.txt
@@ -0,0 +1,7 @@
+Uses Adventure Works Database.
+https://github.com/Microsoft/sql-server-samples/releases/tag/adventureworks
+
+
+
+CrystalReport_Test1
+You must create and grant access to the uspCrystalReport_Test1 stored procedure.
---
+++ Testing/AdventureWorksLt/uspCrystalReport_Test1.sql
@@ -0,0 +1,14 @@
+/****** Object:  StoredProcedure [dbo].[uspCrystalReport_Test1]    Script Date: 9/3/2020 5:20:45 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[uspCrystalReport_Test1]
+
+as
+
+select * from saleslt.Product
+GO
+
